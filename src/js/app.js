808afa43d76f0d8d483a76831ef5832180e63894
voverg <voverg81@mail.ru>
2020-04-20 16:11:41 +0300

    Created add notes to the entryList functionality

--- src/js/app.js
+++ src/js/app.js
@@ -13,24 +13,35 @@ const incomeBtn = document.querySelector('.income-tab');
 const expenseBtn = document.querySelector('.expense-tab');
 const allBtn = document.querySelector('.all-tab');
 // Select the inputs
-const addincomeBtn = document.querySelector('.add-income');
+const incomeAddBtn = document.querySelector('.add-income');
 const incomeTitleInput = document.querySelector('.income-title-input');
 const incomeAmountInput = document.querySelector('.income-amount-input');
 
-const addExpenseBtn = document.querySelector('.add-expense');
+const expenseAddBtn = document.querySelector('.add-expense');
 const expenseTitleInput = document.querySelector('.expense-title-input');
 const expenseAmountInput = document.querySelector('.expense-amount-input');
 // Variables
 let entryList = [];
 let balance = 0, income = 0, outcome = 0;
 const del = 'delete', edit = 'edit';
-// Functions
+
+// --------------- Functions ----------------------------
+function updateUI() {
+
+}
+
+function clearInputs( inputs ) {
+    inputs.forEach(input => {
+        input.value = '';
+    })
+}
+
+// Show and hide tabs and dashboard screens categories
 function show(elem) {
-    console.log(elem);
     elem.classList.remove('hide');
 }
 
-function hide(array) {
+function hide( array ) {
     array.forEach(elem => {
         elem.classList.add('hide');
     });
@@ -40,13 +51,16 @@ function active(tab) {
     tab.classList.add('active');
 }
 
-function inactive(arrayTabs) {
+function inactive( arrayTabs ) {
     arrayTabs.forEach(tab => {
         tab.classList.remove('active');
     });
 }
 
-// Event listeners
+// Add tasks
+
+
+// ------------------------------ Event listeners
 incomeBtn.addEventListener('click', function() {
     show(incomeElem);
     hide( [expenseElem, allElem] );
@@ -64,4 +78,32 @@ allBtn.addEventListener('click', function() {
     hide( [expenseElem, incomeElem] );
     active(allBtn);
     inactive( [expenseBtn, incomeBtn] );
+})
+// Add notes
+incomeAddBtn.addEventListener('click', function() {
+    if (!incomeTitleInput || !incomeAmountInput) return;
+
+    let income = {
+        type: 'income',
+        title: incomeTitleInput.value,
+        amount: incomeAmountInput.value
+    }
+    entryList.push(income);
+
+    updateUI();
+    clearInputs( [incomeTitleInput, incomeAmountInput] )
+})
+
+expenseAddBtn.addEventListener('click', function() {
+    if (!expenseTitleInput || !expenseAmountInput) return;
+
+    let expense = {
+        type: 'expense',
+        title: expenseTitleInput.value,
+        amount: expenseAmountInput.value
+    }
+    entryList.push(expense);
+
+    updateUI();
+    clearInputs( [expenseTitleInput, expenseAmountInput] )
 })
